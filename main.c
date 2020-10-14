66f0b41eaac61eb166863b2c844ab346116cf5fc
Micaela Acuña <micahl1981@gmail.com>
2020-10-14 08:52:37 -0300

    Comenzando!

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+//Estructuras
+typedef struct{
+    int dia;
+    int mes;
+    int anio;
+}eFecha;
+
+typedef struct{
+}eTipo;
+
+typedef struct{
+}eColor;
+
+typedef struct{
+}eBicicleta;
+
+typedef struct{
+}eServicio;
+
+typedef struct{
+}eTrabajo;
 
 int main()
 {
